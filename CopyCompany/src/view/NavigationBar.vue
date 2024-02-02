91804e7eaf2f0c6cab98ad2e7075acc43fcd1f12
linyingdong <yingdonglin7@gmail.com>
2024-02-02 12:20:40 +0800

    初始化

---
+++ CopyCompany/src/view/NavigationBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="navigationBar">
+       
+    </div>
+</template>
+
+<style scoped>
+.navigationBar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    background-color: #5c1919;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+</style>
---
+++ CopyCompany/src/view/TitleBar.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="titleBar">
+        <button @click="goBack">返回</button>
+        <h1>{{ title }} </h1>
+        <button @click="openMenu">菜单</button>
+    </div>
+</template>
+
+<script setup>
+import { ref, defineProps,defineEmits } from "vue";
+
+const title = defineProps({
+    title:String
+});
+
+const emit = defineEmits(["go-back", "open-menu"]);
+
+
+const goBack = () => {
+    console.log("goBack");
+    const data = {
+        name: "zhangsanAA",
+        age: 18,
+    };
+    emit("go-back", data);
+};
+const openMenu = () => {
+    console.log("openMenu");
+    const data = {
+        name: "zhangsanBB",
+        age: 32,
+    };
+    emit("open-menu", data);
+};
+</script>
+
+<style>
+.titleBar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    background-color: #f2f2f2;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+</style>
